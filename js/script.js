b85d6a75bd901b819aa7f6a1410ebe4817e74b77
Abdou Maouda <Abdousa0@hotmail.com>
2025-10-01 20:57:31 -0400

    added a dark mode and a sideBar menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,111 @@
-const menuToggle = document.getElementById("menu-toggle");
+const menuToggle=document.getElementById("menu-toggle");
+const sidebar=document.getElementById("sidebar");
+const closebtn=document.getElementById("close-btn")
+const darkModeTgl=document.getElementById("theme-toggle");
+
+
+
+darkModeTgl.addEventListener("click",event=>{
+  document.body.classList.toggle("darkmode")  
+})
+
+//Method open side bar
+menuToggle.addEventListener("click",event=>{
+  sidebar.classList.toggle("active");
+})
+
+
+//Method to remove side bar
+closebtn.addEventListener("click",event=>{
+  sidebar.classList.remove("active")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const menuToggle = document.getElementById("menu-toggle");
 const sidebar = document.getElementById("sidebar");
 const closeBtn = document.getElementById("close-btn");
 const darkModeTgl=document.getElementById("theme-toggle");
 
 
+
+darkModeTgl.addEventListener("click",()=>{
+  document.body.classList.toggle("darkmode")  
+})
 
 
 
@@ -14,5 +116,5 @@ menuToggle.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   sidebar.classList.remove("active");
 });
-
+*/
 
