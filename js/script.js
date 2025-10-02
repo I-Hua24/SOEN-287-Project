136eb921774d4d49c7be35fc6e2faac83fd1e00f
Abdou Maouda <Abdousa0@hotmail.com>
2025-10-01 22:43:25 -0400

    Improved JS

--- js/script.js
+++ js/script.js
@@ -20,8 +20,28 @@ closebtn.addEventListener("click",event=>{
   sidebar.classList.remove("active")
 })
 
+//Adding the pages to the button
+
+document.getElementById("sign-in").addEventListener("click",()=>{
+window.location.href = "pages/signin.html";
+})
+
+document.getElementById("sign-up").addEventListener("click",()=>{
+  window.location.href="pages/signup.html";
+})
+
+document.getElementById("Browse-Btn").addEventListener("click",()=>{
+  window.location.href="pages/resources.html"
+})
+
+document.getElementById("Admin-btn").addEventListener("click",()=>{
+  window.location.href="pages/adminDashboard.html"
+})
 
 
+document.getElementById("cta-btn").addEventListener("click",()=>{
+  window.location.href="pages/signup.html";
+})
 
 
 
