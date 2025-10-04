8b193663edfc14c5ec65b2a0e7f6999880d9859f
Gaspar Cernacek <gaspar@Gaspars-MacBook-Air.local>
2025-10-04 17:25:30 -0400

    script and css

--- js/script.js
+++ js/script.js
@@ -1,29 +1,27 @@
-const menuToggle=document.getElementById("menu-toggle");
-const sidebar=document.getElementById("sidebar");
-const closebtn=document.getElementById("close-btn")
-const darkModeTgl=document.getElementById("theme-toggle");
+const menuToggle = document.getElementById("menu-toggle");
+const sidebar = document.getElementById("sidebar");
+const closebtn = document.getElementById("close-btn")
+const darkModeTgl = document.getElementById("theme-toggle");
 
-
-
-darkModeTgl.addEventListener("click",event=>{
+darkModeTgl.addEventListener("click", event => {
   document.body.classList.toggle("darkmode")  
 })
 
 //Method open side bar
-menuToggle.addEventListener("click",event=>{
+menuToggle.addEventListener("click", event => {
   sidebar.classList.toggle("active");
 })
 
 
 //Method to remove side bar
-closebtn.addEventListener("click",event=>{
+closebtn.addEventListener("click", event => {
   sidebar.classList.remove("active")
 })
 
 //Adding the pages to the button
 
-document.getElementById("sign-in").addEventListener("click",()=>{
-window.location.href = "pages/signin.html";
+document.getElementById("sign-in").addEventListener("click", () => {
+  window.location.href = "pages/signin.html";
 })
 
 document.getElementById("sign-up").addEventListener("click",()=>{
