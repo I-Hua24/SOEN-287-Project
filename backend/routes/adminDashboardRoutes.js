40a6a0eaeac74c207fcd2c9ca6130593b0c94d8a
Gaspar Cernacek <gaspar@dhcp-172-30-7-240.wireless.concordia.ca>
2025-11-18 17:17:52 -0500

    admin dashboard route

---
+++ backend/routes/adminDashboardRoutes.js
@@ -0,0 +1,22 @@
+// admin dashboard
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import RoomModel from "../model/roomModel.js";
+
+import { verifyTokenMiddleware } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+router.get("/adminDashboard", (req, res) => {
+
+    //   const day = atMidnight();
+    //    /* await */ ensureSeedFor(day, "study", 10);
+
+    res.sendFile(path.join(__dirname, "../../pages/adminDashboard.html"));
+});
+
+
